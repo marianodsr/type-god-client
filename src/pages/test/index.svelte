4272b678abae8f7a7cf3223320de3755a9fd4bb9
marianodsr <marianodsr88@gmail.com>
2021-05-24 20:50:30 -0300

    pre release v5

--- src/pages/test/index.svelte
+++ src/pages/test/index.svelte
@@ -1,1 +1,19 @@
-<h1>Hello from test</h1>
+<h1 on:click={() => callMe()}>Hello from test</h1>
+
+
+
+
+<style>
+    h1 {
+        background-color: red;
+    }
+</style>
+
+
+<script>
+
+
+    const callMe = () => {
+        alert("CALL ME CALLED")
+    }
+</script>
