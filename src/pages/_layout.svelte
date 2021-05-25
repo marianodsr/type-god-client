2321527295116f6f49c0a77f1f40995a31695701
marianodsr <marianodsr88@gmail.com>
2021-05-24 21:15:26 -0300

    metatag description updated

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,7 +1,7 @@
 <script>
     import { metatags } from '@roxi/routify'
     metatags.title = 'Type god'
-    metatags.description = 'Description coming soon...'
+    metatags.description = 'Type god is a game where you race against other people in order to see who can complete a paragraph first'
 </script>
 
 <slot/>
